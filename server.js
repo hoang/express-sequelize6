893b828435fa15845b1f588634ac416748556318
OS10.13 <os10.13@OS1013s-MacBook-Pro.local>
2021-05-19 23:15:35 +0700

    4th commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const cors = require("cors");
 const app = express();
 
 const db = require('./models');
-const {Group} = db;
+const { Group } = db;
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+	origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // simple route
 app.get("/", async (req, res) => {
 	let groups = await Group.findAll();
-  res.json({
+	res.json({
 		message: "Welcome to test application.",
 		groups
 	});
@@ -31,5 +31,5 @@ app.get("/", async (req, res) => {
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+	console.log(`Server is running on port ${PORT}.`);
 });
