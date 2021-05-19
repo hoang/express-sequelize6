0b1a8d28abfc80ab50bb7dc923bfaa480737f4aa
OS10.13 <os10.13@OS1013s-MacBook-Pro.local>
2021-05-19 23:14:51 +0700

    3rd commit

--- models/group.js
+++ models/group.js
@@ -1,35 +1,35 @@
 'use strict';
 const {
-  Model
+	Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Group extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Group.init({
-    group_id: {
+	class Group extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	};
+	Group.init({
+		group_id: {
 			type: DataTypes.INTEGER,
 			primaryKey: true
 		},
-    group_title: DataTypes.STRING,
+		group_title: DataTypes.STRING,
 		group_status: DataTypes.STRING,
 		group_user_id_admin: DataTypes.STRING,
 		group_user_id_create: DataTypes.STRING,
 		created_at: DataTypes.DATE,
 		updated_at: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Group',
+	}, {
+		sequelize,
+		modelName: 'Group',
 		freezeTableName: true,
-    tableName: 'tbl_group',
+		tableName: 'tbl_group',
 		timestamps: false
-  });
-  return Group;
+	});
+	return Group;
 };
